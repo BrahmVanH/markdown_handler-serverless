63496f0039ca7c8d9a15fae5778b9c066e7ecc14
Brahm Van Houzen <brahmvanh@gmail.com>
2024-03-04 02:37:54 -0500

    created form for file upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import sendForm from '../utils/axios';
+import { useForm, FieldValues } from 'react-hook-form';
 
-const  App: React.FC = () => {
-
+interface IFormInput {
+	file: File;
+}
+const App: React.FC = () => {
 	console.log(React);
 
-	const handleSendForm = async (e: React.ChangeEvent<HTMLInputElement>) => {	
+	const [formInput, setFormInput] = React.useState<IFormInput | null>(null);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FieldValues>();
+
+	const handleSetFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const file: File = e.target.files?.[0] as File;
+		setFormInput(file);
+	};
+
+	const handleSendForm = async (formInput: IFormInput) => {
+		const file: File = formInput.file;
 		console.log('file', file);
 		const response = await sendForm(file);
 		console.log('response', response);
-	}
+	};
 
+	useEffect(() => {
+		if (formInput) {
+			console.log('formInput', formInput);
+			handleSendForm(formInput);
+		}
+	}, [formInput]);
+
+	
 	return (
-		
-		<form>
-			<input type="file" name="file" onChange={handleSendForm} />
+		<form encType='multipart/form-data'>
+			<input type='file' {...register('file', { required: { value: true, message: 'all fields are required' } })} />
+			<button type='submit'>Submit</button>
 		</form>
 	);
-}
+};
 
 export default App;
