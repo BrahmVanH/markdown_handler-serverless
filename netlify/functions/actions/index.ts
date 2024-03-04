3768518e8ff7aae0a63a56e36eac872328341f62
Brahm Van Houzen <brahmvanh@gmail.com>
2024-03-03 21:45:09 -0500

    handler stopped working after building rest of app. Backtracked a little and am slowly working bits back in

--- netlify/functions/actions/index.ts
+++ netlify/functions/actions/index.ts
@@ -1,16 +1,24 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import serverless from 'serverless-http';
 import dotenv from 'dotenv';
+import router from './router';
 
-import router from './router.mjs';
+// import router from './router.mjs';
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
+// const router = express.Router();
 
-app.use(function (req: Request, res: Response, next) {
+interface IRequest extends Request {
+	headers: {
+		host?: string;
+	};
+}
+
+const getAllowedOrigins = (req: IRequest, res: Response, next: NextFunction) => {
 	console.log('req.headers', req.headers);
-	const allowedOrigins = ['http://localhost:8888', 'https://markdown-handler.netlify.app'];
+	const allowedOrigins = ['localhost:8888', 'https://markdown-handler.netlify.app'];
 	const host = req.headers.host;
 	console.log('host', host);
 
@@ -19,9 +27,24 @@ app.use(function (req: Request, res: Response, next) {
 	} else {
 		res.status(405).send('Host not allowed');
 	}
-});
+};
+app.use(getAllowedOrigins);
+
+// router.get('/', (req, res) => {
+// 	res.writeHead(200, { 'Content-Type': 'text/html' });
+// 	res.write('Hello, actionszzzz');
+// 	res.end();
+// });
+
+// router.get('/like', (req, res) => {
+// 	res.status(200).json({ action: 'like' });
+// });
 
 app.use('/.netlify/functions/actions', router);
 
 const handler = serverless(app);
-export default handler;
+
+export { handler };
+
+// router.get('/', (req: Request, res: Response) => console.log('Hello from Express.js!'));
+// app.use('api', router);
---
+++ netlify/functions/actions/router.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from 'express';
+import connectToDb from '../../../src/db';
+import getActionsModel from './EntryModel';
+
+const router = express.Router();
+
+// const incrementAction = async (req: Request, res: Response, action: string, url: string) => {
+// 	console.log('incrementAction', action, url);
+// 	await connectToDb();
+
+// 	const MongooseModelActions = await getActionsModel();
+
+// 	if (!MongooseModelActions) {
+// 		res.status(500).json({ error: 'Internal Server Error' });
+// 		return;
+// 	}
+
+// 	try {
+// 		const actionResult = await MongooseModelActions.findOneAndUpdate({ url }, { $inc: { [action]: 1 } }, { new: true, upsert: true });
+
+// 		res.status(200).json(actionResult);
+// 	} catch (error) {
+// 		res.status(500).json({ error: 'Internal Server Error' });
+// 	}
+// };
+
+router.get('/', (req: Request, res: Response) => {
+	console.log('Hello from Express.js!');
+	res.status(200).json({ message: 'Hello from Express.js!' });
+});
+
+// router.get('/like', async (req: Request, res: Response) => {
+// 	console.log('like', req.query.url);
+// 	const url = req.query.url as string;
+
+// 	if (!url) {
+// 		res.status(400).json({ error: 'Bad Request' });
+// 	} else {
+// 		await incrementAction(req, res, 'like', url);
+// 	}
+// });
+
+export default router;
